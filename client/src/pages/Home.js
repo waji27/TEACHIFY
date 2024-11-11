6f2d87e172966cd5aafd4755289ced19b4894c6a
aalleey <alibinabdulkhaliq50@gmail.com>
2024-11-12 00:52:17 +0500

    error home is not defined

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,31 +2,115 @@ import React from 'react'
 import Layout from '../components/Layout'
 import Carousel from '../components/Carousel'
 
-const Home = () => {
+function Navbar() {
     return (
-        <Layout>
-            <Carousel />
-            <div class="contain w-full bg-gray-50 h-[40vh] border ">
-                <div class="first-h">
-                    <h1>High Quality Teacher</h1>
-                </div>
-                <div class="second-h">
-                    <h1>teachers that apply make through our application process</h1>
-                </div>
-            </div>
-            <div class="c w-full h-[20vh] flex justify-evenly ">
-                <div class="one w-2">
-                    100+ Subjects
-                </div>
-                <div class="one w-2">
-                    200+ skills
-                </div>
-                <div class="one w-2"> 
-                    50+ languages
-                </div>
-            </div>
-        </Layout>
-    )
-}
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+          </a>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+  
+  function TeachifySection() {
+    return (
+      <div className="containerr">
+        <div className="pg-l">
+          <img src="/rafiki.png" alt="image" />
+        </div>
+        <div className="pg-r">
+          <div className="midd">
+            <h1>Welcome to Teachify – A New Way to Connect with Educators!</h1>
+            <p>
+              Teachify is an innovative platform designed to bridge the gap between students seeking knowledge and teachers
+              eager to share their expertise. Whether you're a student in search of a personalized learning experience or
+              an educator looking to empower learners, Teachify makes it easy to connect and collaborate.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  function App() {
+    return (
+      <>
+        <Navbar />
+        <TeachifySection />
+      </>
+    );
+  }
 
 export default Home
