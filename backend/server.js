2fcdedc4bd796e8e17247122fa3b3e72ce90cc8d
waji27 <wajdanakmal37@gmail.com>
2024-11-12 18:17:32 +0500

    started working on baackend, basic server file is ready, database configuring function ready.

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+// import authRoutes from "./routes/authRoute.js";
+
+// configuring dotenv
+dotenv.config();
+
+// configuring databse connection
+connectDB();
+
+//creating app with express
+const app = express();
+
+// configuring middelwares
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+//rest api
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to ecommerce app</h1>");
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+    console.log(
+        `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+            .white
+    );
+});
