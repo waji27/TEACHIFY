d5ab8861b64ab2d90c05316c41f1b83e7fc481c3
waji27 <wajdanakmal37@gmail.com>
2024-11-18 01:45:15 +0500

    dropdown working

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,151 +2,154 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png"; // Adjust the logo import path as needed.
 
-function LoginDropdown() {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-}
 
 function Navbar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-2.5">
-        <Link
-          to="/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img src={logo} className="h-8" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Teachify
-          </span>
-        </Link>
-
-        {/* Mobile menu button */}
-        <button
-          onClick={() => setIsNavOpen(!isNavOpen)}
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-dropdown"
-          aria-expanded={isNavOpen}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
+    <>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-2.5">
+          <Link
+            to="/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
+            <img src={logo} className="h-8" alt="Flowbite Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Teachify
+            </span>
+          </Link>
 
-        {/* Navbar items */}
-        <div
-          className={`${
-            isNavOpen ? "block" : "hidden"
-          } w-full md:block md:w-auto`}
-          id="navbar-dropdown"
-        >
-          <ul className="flex flex-col font-medium p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:mt-0 md:text-sm md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 md:space-x-8 md:rtl:space-x-reverse">
-            <li>
-              <Link
-                to="/store"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-              >
-                Store
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/tutors"
-                className="block py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white"
-              >
-                Tutors
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/services"
-                className="block py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white"
-              >
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/pricing"
-                className="block py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white"
-              >
-                Pricing
-              </Link>
-            </li>
-            <li>
-              <button
-                id="dropdownDefaultButton"
-                data-dropdown-toggle="dropdown"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                type="button"
-              >
-                Login/SignUp{" "}
-                <svg
-                  class="w-2.5 h-2.5 ms-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 10 6"
+          {/* Mobile menu button */}
+          <button
+            onClick={() => setIsNavOpen(!isNavOpen)}
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-dropdown"
+            aria-expanded={isNavOpen}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+
+          {/* Navbar items */}
+          <div
+            className={`${isNavOpen ? "block" : "hidden"
+              } w-full md:block md:w-auto`}
+            id="navbar-dropdown"
+          >
+            <ul className="flex flex-col font-medium p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:mt-0 md:text-sm md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 md:space-x-8 md:rtl:space-x-reverse">
+              <li>
+                <Link
+                  to="/store"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                 >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m1 1 4 4 4-4"
-                  />
-                </svg>
-              </button>
-
-              <div
-                id="dropdown"
-                class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-              >
-                <ul
-                  class="py-2 text-sm text-gray-700 dark:text-gray-200"
-                  aria-labelledby="dropdownDefaultButton"
+                  Store
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/tutors"
+                  className="block py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white"
                 >
-                  <li>
-                    <a
-                      href="/login"
-                      class=" px-4 py-2 flex justify-center text-center  hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Login
-                    </a>
-                  </li>
+                  Tutors
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/services"
+                  className="block py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white"
+                >
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/pricing"
+                  className="block py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white"
+                >
+                  Pricing
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                  id="dropdownDefaultButton"
+                  data-dropdown-toggle="dropdown"
+                  class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  type="button"
+                >
+                  Login/SignUp{" "}
+                  <svg
+                    class="w-2.5 h-2.5 ms-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 10 6"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m1 1 4 4 4-4"
+                    />
+                  </svg>
+                </button>
 
-                  <span className="flex justify-center font-light">OR</span>
-                  <li>
-                    <a
-                      href="/signup"
-                      class="flex justify-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      SignUp
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-          </ul>
+
+              </li>
+            </ul>
+          </div>
         </div>
+      </nav>
+      <div
+        id="dropdown"
+        className={`${isDropdownOpen ? "block" : "hidden"
+          } z-10 w-44 divide-y divide-gray-100 overflow-hidden overflow-y-auto rounded-lg bg-white antialiased shadow dark:divide-gray-600 dark:bg-gray-700`}
+      >
+        <ul
+          class="py-2 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownDefaultButton"
+        >
+          <li>
+            <Link
+              to="/login"
+              class=" px-4 py-2 flex justify-center text-center  hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Login
+            </Link>
+          </li>
+
+          <span className="flex justify-center font-light">OR</span>
+          <li>
+            <Link
+              to="/signup"
+              class="flex justify-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              SignUp
+            </Link>
+          </li>
+        </ul>
       </div>
-    </nav>
+
+
+    </>
   );
 }
 
--- client/src/pages/Auth/Signup.js
+++ client/src/pages/Auth/Signup.js
@@ -6,38 +6,38 @@ import styled from 'styled-components';
 const Signup = () => {
   return (
     <Layout >
-     <StyledWrapper>
-      <form className="form">
-        <p className="title">Register </p>
-        <p className="message">Signup now and get full access to our app. </p>
-        <div className="flex">
+      <StyledWrapper>
+        <form className="form">
+          <p className="title">Register </p>
+          <p className="message">Signup now and get full access to our app. </p>
+          <div className="flex">
+            <label>
+              <input required placeholder type="text" className="input" />
+              <span>Firstname</span>
+            </label>
+            <label>
+              <input required placeholder type="text" className="input" />
+              <span>Lastname</span>
+            </label>
+          </div>
           <label>
-            <input required placeholder type="text" className="input" />
-            <span>Firstname</span>
+            <input required placeholder type="email" className="input" />
+            <span>Email</span>
           </label>
           <label>
-            <input required placeholder type="text" className="input" />
-            <span>Lastname</span>
+            <input required placeholder type="password" className="input" />
+            <span>Password</span>
           </label>
-        </div>  
-        <label>
-          <input required placeholder type="email" className="input" />
-          <span>Email</span>
-        </label> 
-        <label>
-          <input required placeholder type="password" className="input" />
-          <span>Password</span>
-        </label>
-        <label>
-          <input required placeholder type="password" className="input" />
-          <span>Confirm password</span>
-        </label>
-        <button className="submit">Submit</button>
-        <p className="signin">Already have an acount ? <a href="#">Signin</a> </p>
-      </form>
-      
-    </StyledWrapper>
-      
+          <label>
+            <input required placeholder type="password" className="input" />
+            <span>Confirm password</span>
+          </label>
+          <button className="submit">Submit</button>
+          <p className="signin">Already have an acount ? <a href="#">Signin</a> </p>
+        </form>
+
+      </StyledWrapper>
+
     </Layout>
   );
 };
