67290071ddc54ec79a93d253adf4cbc5ad8655e5
aalleey <alibinabdulkhaliq50@gmail.com>
2024-11-30 00:20:06 +0500

    some change

--- client/src/pages/store.js
+++ client/src/pages/store.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Storeserch from '../components/storesearch'
 
 
 const Store = () => {
   return (
         <Layout>
+            <Storeserch />
 
 
 
